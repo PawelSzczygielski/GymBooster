1f1e4484be844f45fc3cbb0216c3ab526fa570bc
Pawel Szczygielski <pawel.szczygielski@banqsoft.no>
2020-07-14 13:36:01 +0200

    Initial Xamarin project added

---
+++ GymBooster.AndroidApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using GymBooster.AndroidApp.Services;
+using GymBooster.AndroidApp.Views;
+
+namespace GymBooster.AndroidApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ GymBooster.AndroidApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using GymBooster.AndroidApp.Models;
+
+namespace GymBooster.AndroidApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
