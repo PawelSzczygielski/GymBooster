83b69caa2c21287cafe67137da84d553b3de8188
Pawel Szczygielski <pawel.szczygielski@banqsoft.no>
2020-09-18 13:53:29 +0200

    Displaying in Android app, beginning. TrainingDTO probably should not be used in Android app. But it's right now.

--- GymBooster.AndroidApp/Services/MockDataStore.cs
+++ GymBooster.AndroidApp/Services/MockDataStore.cs
@@ -1,54 +1,60 @@
-﻿namespace GymBooster.AndroidApp.Services
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GymBooster.Common.Objects.DTO;
+
+namespace GymBooster.AndroidApp.Services
 {
-    //public class MockDataStore : IDataStore<Item>
-    //{
-    //    readonly List<Item> items;
+    public class MockDataStore : IDataStore<TrainingDTO>
+    {
+        readonly List<TrainingDTO> items;
 
-    //    public MockDataStore()
-    //    {
-    //        items = new List<Item>()
-    //        {
-    //            new Item { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description." },
-    //            new Item { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description." },
-    //            new Item { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description." },
-    //            new Item { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description." },
-    //            new Item { Id = Guid.NewGuid().ToString(), Text = "Fifth item", Description="This is an item description." },
-    //            new Item { Id = Guid.NewGuid().ToString(), Text = "Sixth item", Description="This is an item description." }
-    //        };
-    //    }
+        public MockDataStore()
+        {
+            items = new List<TrainingDTO>()
+            {
+                new TrainingDTO(Guid.NewGuid().ToString(),    "First item", new List<ExerciseDTO>()),
+                new TrainingDTO (Guid.NewGuid().ToString(), "Second item", new List<ExerciseDTO>()),
+                new TrainingDTO (Guid.NewGuid().ToString(), "Third item", new List<ExerciseDTO>()),
+                new TrainingDTO (Guid.NewGuid().ToString(), "Fourth item", new List<ExerciseDTO>()),
+                new TrainingDTO (Guid.NewGuid().ToString(), "Fifth item", new List<ExerciseDTO>()),
+                new TrainingDTO (Guid.NewGuid().ToString(), "Sixth item", new List<ExerciseDTO>()),
+            };
+        }
 
-    //    public async Task<bool> AddItemAsync(Item item)
-    //    {
-    //        items.Add(item);
+        public async Task<bool> AddItemAsync(TrainingDTO item)
+        {
+            items.Add(item);
 
-    //        return await Task.FromResult(true);
-    //    }
+            return await Task.FromResult(true);
+        }
 
-    //    public async Task<bool> UpdateItemAsync(Item item)
-    //    {
-    //        var oldItem = items.Where((Item arg) => arg.Id == item.Id).FirstOrDefault();
-    //        items.Remove(oldItem);
-    //        items.Add(item);
+        public async Task<bool> UpdateItemAsync(TrainingDTO item)
+        {
+            var oldItem = items.Where((TrainingDTO arg) => arg.Id == item.Id).FirstOrDefault();
+            items.Remove(oldItem);
+            items.Add(item);
 
-    //        return await Task.FromResult(true);
-    //    }
+            return await Task.FromResult(true);
+        }
 
-    //    public async Task<bool> DeleteItemAsync(string id)
-    //    {
-    //        var oldItem = items.Where((Item arg) => arg.Id == id).FirstOrDefault();
-    //        items.Remove(oldItem);
+        public async Task<bool> DeleteItemAsync(string id)
+        {
+            var oldItem = items.Where((TrainingDTO arg) => arg.Id == id).FirstOrDefault();
+            items.Remove(oldItem);
 
-    //        return await Task.FromResult(true);
-    //    }
+            return await Task.FromResult(true);
+        }
 
-    //    public async Task<Item> GetItemAsync(string id)
-    //    {
-    //        return await Task.FromResult(items.FirstOrDefault(s => s.Id == id));
-    //    }
+        public async Task<TrainingDTO> GetItemAsync(string id)
+        {
+            return await Task.FromResult(items.FirstOrDefault(s => s.Id == id));
+        }
 
-    //    public async Task<IEnumerable<Item>> GetItemsAsync(bool forceRefresh = false)
-    //    {
-    //        return await Task.FromResult(items);
-    //    }
-    //}
+        public async Task<IEnumerable<TrainingDTO>> GetItemsAsync(bool forceRefresh = false)
+        {
+            return await Task.FromResult(items);
+        }
+    }
 }
--- GymBooster.AndroidApp/ViewModels/ItemsViewModel.cs
+++ GymBooster.AndroidApp/ViewModels/ItemsViewModel.cs
@@ -2,28 +2,31 @@
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Threading.Tasks;
-
+using GymBooster.AndroidApp.Services;
+using GymBooster.AndroidApp.Views;
+using GymBooster.Common.Objects.DTO;
 using Xamarin.Forms;
 
 namespace GymBooster.AndroidApp.ViewModels
 {
     public class ItemsViewModel : BaseViewModel
     {
-        public ObservableCollection<string> Items { get; set; }
+        public ObservableCollection<TrainingDTO> Items { get; set; }
         public Command LoadItemsCommand { get; set; }
 
         public ItemsViewModel()
         {
-            //Title = "Browse";
-            //Items = new ObservableCollection<Item>();
-            //LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+            
+            Title = "Browse";
+            Items = new ObservableCollection<TrainingDTO>();
+            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
 
-            //MessagingCenter.Subscribe<NewItemPage, Item>(this, "AddItem", async (obj, item) =>
-            //{
-            //    var newItem = item as Item;
-            //    Items.Add(newItem);
-            //    await DataStore.AddItemAsync(newItem);
-            //});
+            MessagingCenter.Subscribe<NewItemPage, TrainingDTO>(this, "AddItem", async (obj, item) =>
+            {
+                var newItem = item as TrainingDTO;
+                Items.Add(newItem);
+                await DataStore.AddItemAsync(newItem);
+            });
         }
 
         async Task ExecuteLoadItemsCommand()
@@ -32,12 +35,12 @@ namespace GymBooster.AndroidApp.ViewModels
 
             try
             {
-                //Items.Clear();
-                //var items = await DataStore.GetItemsAsync(true);
-                //foreach (var item in items)
-                //{
-                //    Items.Add(item);
-                //}
+                Items.Clear();
+                var items = await DataStore.GetItemsAsync(true);
+                foreach (var item in items)
+                {
+                    Items.Add(item);
+                }
             }
             catch (Exception ex)
             {
